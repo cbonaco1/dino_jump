e4603b393cd3c977a45460776c381c61f483398d
cbonaco1 <christian.bonacore@gmail.com>
2016-02-09 09:55:38 -0500

    Score displays, pressing spacebar starts score tracker, pressing it again resets to 0

--- dinoGame.js
+++ dinoGame.js
@@ -1,4 +1,21 @@
 var Game = require('./javascript/game');
 
-var game = new Game();
-game.hello();
+$(document).ready(function() {
+  var game = new Game();
+  var dino = document.getElementById("dino");
+
+  //dont need to put this in a document ready since it is on the document
+  document.addEventListener("keypress", function(e) {
+    if (e.keyCode === 32) {
+      e.preventDefault();
+      if (!game.started) {
+        game.start();
+      }
+      else {
+        game.stop();
+      }
+    }
+  });
+
+
+});
--- javascript/game.js
+++ javascript/game.js
@@ -1,5 +1,6 @@
 var Game = function() {
   this.score = 0;
+  this.started = false;
 }
 
 Game.prototype.hello = function () {
@@ -8,16 +9,20 @@ Game.prototype.hello = function () {
 
 Game.prototype.start = function () {
   console.log("Game has started, score is: " + this.score);
-  this.timeInterval = window.setInterval(this.incrementScore.bind(this), 2000);
+  this.started = true;
+  this.timeInterval = window.setInterval(this.incrementScore.bind(this), 500);
 };
 
 Game.prototype.incrementScore = function () {
   this.score += 1;
-  console.log("Score is now: " + this.score);
+  // console.log("Score is now: " + this.score);
+  var scoreLabel = document.getElementById("score");
+  scoreLabel.innerHTML = this.score;
 };
 
 Game.prototype.stop = function () {
   window.clearInterval(this.timeInterval);
+  this.started = false;
   this.score = 0;
 };
 
